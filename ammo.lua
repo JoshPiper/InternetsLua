18f8b683a89e4eba81977d9af4e5d7c72f8b0a5f
Doctor Internet <jonjon1234.github@gmail.com>
2015-03-17 10:31:57 +0000

    Ammo Updated
    Updates stripammo to properly strip ammo, include ammo in clips (it
    wipes all ammo, instead of just reserve ammo)

--- ammo.lua
+++ ammo.lua
@@ -1,5 +1,5 @@
 --[[
-ULX Ammo Module
+ULX Ammo/Weapons Module
 	Unlimited Ammo
 	Set Ammo Amount
 	Set Current Ammo
@@ -13,7 +13,7 @@ local CATEGORY_NAME = "Ammo"
 
 ------ Strip Ammunition ------
 function ulx.stripAmmo( calling_ply, target_plys )
-	if not target_ply then
+	if not target_plys then
 		target_plys = {}
 		table.insert(target_plys, calling_ply)
 	end
@@ -21,11 +21,32 @@ function ulx.stripAmmo( calling_ply, target_plys )
 	for i = 1, #target_plys do
 		local v = target_plys[ i ]
 		v:StripAmmo()
+		for _, i in pairs(v:GetWeapons()) do
+			i:SetClip1(0)
+			i:SetClip2(0)
+		end
 		table.insert( affected_plys, v )
 	end
 	ulx.fancyLogAdmin( calling_ply, "#A stripped ammo from #T", affected_plys )
 end
-local stripam = ulx.command( CATEGORY_NAME, "ulx stripammo", ulx.noclip, "!stripammo" )
+local stripam = ulx.command( CATEGORY_NAME, "ulx stripammo", ulx.stripAmmo, "!stripammo" )
 stripam:addParam{ type=ULib.cmds.PlayersArg, ULib.cmds.optional }
 stripam:defaultAccess( ULib.ACCESS_ADMIN )
 stripam:help( "Strips the ammo from target player(s)." )
+
+------ Set Current Ammo ------
+function ulx.setCurrentAmmo (calling_ply, target_plys, amount)
+	if not target_plys then
+		target_plys = {}
+		table.insert(target_plys, calling_ply)
+	end
+	local affected_plys = {}
+	for i = 1, #target_plys do
+		local v = target_plys[i]
+		v:GetActiveWeapon():SetAmmo(amount)
+	end
+end
+local stripamd = ulx.command( CATEGORY_NAME, "ulx stripammod", ulx.setCurrentAmmo, "!stripammod" )
+stripamd:addParam{ type=ULib.cmds.PlayersArg, ULib.cmds.optional }
+stripamd:defaultAccess( ULib.ACCESS_ADMIN )
+stripamd:help( "Strips the ammo from target player(s)." )
